4f478ae1018b4dac814ba2a1263e52d524f70abc
Dominic van Berkel <dominic@baudvine.net>
2018-04-23 17:01:24 +0200

    first commit!

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+#![deny(missing_docs)]
+
+//! A crate for reading bitfields that are defined at runtime.
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
