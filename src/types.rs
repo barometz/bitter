b91369a788e703b2d94e72d933248cfc66ea281e
Dominic van Berkel <dominic@baudvine.net>
2018-05-13 13:49:17 +0200

    first sketch of new approach

--- src/types.rs
+++ src/types.rs
@@ -1,18 +1,63 @@
 //! Types used to describe bitfields. These describe structure and do not hold any actual values.
 
-use std::collections::HashMap;
+/// A single bit field, representing an individual value
+#[derive(Debug)]
+pub struct Field {
+    /// The name of this field
+    pub name: String,
+    /// The offset in bits from 0 at which this field starts
+    pub offset: usize,
+    /// The size in bits
+    pub bits: usize,
+}
 
-/// The size of a single bit field.
-pub type FieldSize = usize;
-/// The name of a bit field.
-pub type FieldName = String;
-/// A mapping between enum values and descriptions
-pub type EnumMapping = HashMap<usize, String>;
+impl Field {
+    /// Create a new Field with a name but no data
+    ///
+    /// ```
+    /// use bitview::Field;
+    ///
+    /// let field = Field::new("OSR");
+    /// assert_eq!(field.name, String::from("OSR"));
+    /// ```
+    pub fn new(name: &str) -> Field {
+        Field {
+            name: name.into(),
+            offset: 0,
+            bits: 0,
+        }
+    }
+
+    /// Give a field a size
+    ///
+    /// ```
+    /// use bitview::Field;
+    ///
+    /// let field = Field::new("OSR").downto(14, 7);
+    /// assert_eq!(field.offset, 7);
+    /// assert_eq!(field.bits, 8);
+    /// ```
+    pub fn downto(self, top: usize, bottom: usize) -> Field {
+        Field {
+            offset: bottom,
+            bits: top - bottom + 1,
+            ..self
+        }
+    }
+}
+
+/// A structure of bit fields
+pub struct Structure {
+    /// The name of the structure
+    pub name: String,
+    /// The fields that make up the structure.
+    ///
+    /// The fields don't have to be contiguous, but the names must be unique.
+    pub fields: Vec<Field>,
+}
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
--- src/values.rs
+++ src/values.rs
@@ -1,15 +1,7 @@
 //! Represent values.
 
-use types;
-
-fn mask(bits: usize) -> usize {
-    2_usize.pow(bits as u32) - 1
-}
-
 #[cfg(test)]
 mod tests {
-    use super::*;
-
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
